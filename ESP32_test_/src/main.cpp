7618452dd7bea094d4f05b9e07b235ec76fe360c
macscEE <bax.random@gmail.com>
2025-11-04 14:01:16 +0100

    Test MPU6050
    First test of communication with IMU

--- ESP32_test_/src/main.cpp
+++ ESP32_test_/src/main.cpp
@@ -1,16 +1,53 @@
+// Load necessary libraries
+
 #include <Arduino.h>
+#include <Wire.h>
+#include <MPU6050.h>
 
+// Define pin numbers
+#define SDA_PIN 21
+#define SCL_PIN 22
 
-#define LED 13
+// Create an MPU6050 object
+MPU6050 mpu;
 
 void setup() {
-  pinMode(LED, OUTPUT);
+  pinMode(SDA_PIN, INPUT);
+  pinMode(SCL_PIN, INPUT);
+
+  Serial.begin(115200);
+  Wire.begin();
+
+  Serial.println("Inizializzazione MPU6050...");
+  mpu.initialize();
+
+  if (!mpu.testConnection()) {
+    Serial.println("Connessione al MPU6050 fallita!");
+    while (1);
+  }
+  Serial.println("MPU6050 connesso correttamente!");
   
+  // Calibra i sensori
+  mpu.CalibrateAccel(6);  // 6 = numero di campioni
+  mpu.CalibrateGyro(6);
+
+  Serial.println("Calibrazione completata!");
+  mpu.PrintActiveOffsets();  // Mostra gli offset calcolati
+
+  delay(1000);
 }
 
 void loop() {
-  digitalWrite(LED, LOW);
-  delay(50);
-  digitalWrite(LED, HIGH);
-  delay(50);
+  int16_t ax, ay, az, gx, gy, gz;
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  Serial.print("a/g:\t");
+  Serial.print(ax); Serial.print("\t");
+  Serial.print(ay); Serial.print("\t");
+  Serial.print(az); Serial.print("\t");
+  Serial.print(gx); Serial.print("\t");
+  Serial.print(gy); Serial.print("\t");
+  Serial.println(gz);
+
+  delay(500);
 }
