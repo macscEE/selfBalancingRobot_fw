5e254746acf1418ab04162843d81a643ed6ce03d
macscEE <bax.random@gmail.com>
2025-11-11 14:13:46 +0100

    minor changer

--- ESP32_test_/src/main.cpp
+++ ESP32_test_/src/main.cpp
@@ -3,13 +3,24 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <MPU6050.h>
+#include <PID_v1.h>
 
 // Define pin numbers
 #define SDA_PIN 21
 #define SCL_PIN 22
 
+// Define PID parameters
+// Input => Measured value
+// Output => Voltage applied to motor
+// Setpoint => Desired value of angle, stable at pi [radians] = 180 degrees
+double Setpoint, Input, Output;
+double Kp=2, Ki=5, Kd=1;
+
 // Create an MPU6050 object
 MPU6050 mpu;
+
+// Create a PID controller object
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 void setup() {
   pinMode(SDA_PIN, INPUT);
@@ -34,20 +45,18 @@ void setup() {
   Serial.println("Calibrazione completata!");
   mpu.PrintActiveOffsets();  // Mostra gli offset calcolati
 
+  // Imposta il setpoint iniziale
+  Setpoint = 180; // Target angle in degrees
+  myPID.SetMode(AUTOMATIC); // Attiva il PID controller
+
+  Serial.println("Setup completato!");
   delay(1000);
 }
 
 void loop() {
   int16_t ax, ay, az, gx, gy, gz;
-  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-  Serial.print("a/g:\t");
-  Serial.print(ax); Serial.print("\t");
-  Serial.print(ay); Serial.print("\t");
-  Serial.print(az); Serial.print("\t");
-  Serial.print(gx); Serial.print("\t");
-  Serial.print(gy); Serial.print("\t");
-  Serial.println(gz);
+  // Input of PID will be the y-axis angle
+  // Input = mpu.getAngleY(&ay);
 
-  delay(500);
 }
